2b123dff2b47e8514fb78fbf8fe8fb3a60eb4e9a
ClintPy <cmaruti93@gmail.com>
2019-08-18 22:08:25 +0300

    Finishes Nav Links & CSS

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     return(
         <nav className="nav-wrapper gret darken-3">
             <div className="container">
-                <Link to='/' className="brand-logo">MarioPlan</Link>
+                <Link to='/' className="brand-logo">Mpango</Link>
                 <SignedInLinks />
                 <SignedOutLinks />
             </div>
