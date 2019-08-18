6f61788820aeddc1eaf499094961e074583ee3ee
ClintPy <cmaruti93@gmail.com>
2019-08-18 21:31:09 +0300

    Finishes create project Form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dashboard from './component/dashboard/Dashboard';
 import ProjectDetails from './component/projects/ProjectDetails'
 import SignIn from './component/auth/SignIn'
 import SignUp from './component/auth/SignUp'
-
+import CreateProject from './component/projects/createProject'
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
           <Route path='/signup' component={SignUp} />
+          <Route path='/create' component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
