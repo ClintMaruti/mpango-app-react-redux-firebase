1ca3a8d364eca040081259b2ea7ad1dc6e463a71
ClintPy <cmaruti93@gmail.com>
2019-08-18 21:12:56 +0300

    Finishes Login & Signup Components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './component/layout/Navbar'
 import Dashboard from './component/dashboard/Dashboard';
 import ProjectDetails from './component/projects/ProjectDetails'
+import SignIn from './component/auth/SignIn'
+import SignUp from './component/auth/SignUp'
+
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
