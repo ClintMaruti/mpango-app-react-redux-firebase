1a46ddce1e2befd5c573d70c1e41bfb27d752fed
ClintPy <cmaruti93@gmail.com>
2019-08-19 11:13:05 +0300

    Finishes Adding Dummy Data

--- src/component/projects/ProjectSummary.js
+++ src/component/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                     <p>Posted by Clint Abraham</p>
                     <p className="grey-text">18th August 2019, 8 p.m</p>
             </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const iniState = {}
+const iniState = {
+    projects: [
+        {id: 1, title: 'My city', content: 'blah blah blah'},
+        {id: 2, title: 'Portmore', content: 'blah blah blah'},
+        {id: 3, title: 'Merch', content: 'blah blah blah'},
+    ]
+}
 
 const projectReducer = (state = iniState, action) => {
     return state
