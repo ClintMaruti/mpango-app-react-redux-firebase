0f12dfee93a9b39c416c87b57b21fd9c1a0151b7
ClintPy <cmaruti93@gmail.com>
2019-08-20 15:25:10 +0300

    Fix create project form

--- src/component/projects/ProjectSummary.js
+++ src/component/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({project}) => {
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text-darken-3">
                 <span className="card-title">{project.title}</span>
-                    <p>Posted by Clint Abraham</p>
+                    <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
                     <p className="grey-text">18th August 2019, 8 p.m</p>
             </div>
         </div>
