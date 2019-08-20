401a5110fe2c83715ce7de1e972c3afbd21e854c
ClintPy <cmaruti93@gmail.com>
2019-08-20 22:18:25 +0300

    Ready for deployment

---
+++ dist/precache-manifest.e1c9580ec7fe4b128a2190bd7bd5caaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1979a2d070829c81510251826fbac53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d9bcb9578bd879b3317",
+    "url": "/static/css/main.efa7fe4d.chunk.css"
+  },
+  {
+    "revision": "693394239e7b23533528",
+    "url": "/static/js/2.6b7156b1.chunk.js"
+  },
+  {
+    "revision": "6d9bcb9578bd879b3317",
+    "url": "/static/js/main.da8d4ec6.chunk.js"
+  },
+  {
+    "revision": "504e585bfb74b6f9b885",
+    "url": "/static/js/runtime~main.505240fb.js"
+  }
+]);
