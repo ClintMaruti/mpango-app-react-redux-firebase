217545a20e7f8e95dd6ccfed95676e6e2b82fea1
ClintPy <cmaruti93@gmail.com>
2019-08-21 16:29:22 +0300

    Updates

---
+++ precache-manifest.7a005c2300baa3e0f22d42653820ec81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40ee3c181896fed72be8ee3a037ba34",
+    "url": "/ClintPy/Mario-Plan-React-Redux-Firebase.git/index.html"
+  },
+  {
+    "revision": "40ec4e8968799d707c05",
+    "url": "/ClintPy/Mario-Plan-React-Redux-Firebase.git/static/css/main.efa7fe4d.chunk.css"
+  },
+  {
+    "revision": "0f67e2a89266024cde6c",
+    "url": "/ClintPy/Mario-Plan-React-Redux-Firebase.git/static/js/2.a034e8f1.chunk.js"
+  },
+  {
+    "revision": "40ec4e8968799d707c05",
+    "url": "/ClintPy/Mario-Plan-React-Redux-Firebase.git/static/js/main.98045157.chunk.js"
+  },
+  {
+    "revision": "35b6f00fffdf7af325fa",
+    "url": "/ClintPy/Mario-Plan-React-Redux-Firebase.git/static/js/runtime~main.6caf33ed.js"
+  }
+]);
